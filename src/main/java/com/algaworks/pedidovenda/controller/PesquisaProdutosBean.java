151cca885d5bf033f607c1d3d936de067b183187
vitorfelipep <vitorfelipep@gmail.com>
2015-12-01 23:03:55 -0200

    Implementação do CDI

--- src/main/java/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
+++ src/main/java/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
@@ -1,14 +1,18 @@
 package com.algaworks.pedidovenda.controller;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
 
-@ManagedBean
+@Named
 @ViewScoped
-public class PesquisaProdutosBean {
+public class PesquisaProdutosBean implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
 	
 	private  List<Integer> produtosFiltrados;
 	
